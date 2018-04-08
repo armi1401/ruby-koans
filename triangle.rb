34383ce8c16c1146b33408b0d6cd1617724e3232
armi1401 <armi1401@gmail.com>
2018-04-08 17:35:35 +0200

    Completed about_triangle_project_2.rb

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+
+  if a <= 0 || b <= 0 || c <= 0
+  	raise TriangleError
+  elsif (a+b<=c) || (a+c<=b) || (b+c<=a)
+  	raise TriangleError
+  end	
+
   if (a == b) && (a == c)
   	return :equilateral
   elsif (a == b) || (a == c) || (b == c)
